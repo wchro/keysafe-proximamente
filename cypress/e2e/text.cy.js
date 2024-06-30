324eee14db39c361732ce9577fb78f679d8a8f73
wchro <93561159+wchro@users.noreply.github.com>
2024-07-01 00:27:40 +0200

    CD added

--- cypress/e2e/text.cy.js
+++ cypress/e2e/text.cy.js
@@ -4,9 +4,6 @@ describe("Check Coming Soon", () => {
   });
 
   it("display coming soon text", () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
     cy.contains("Muy pronto");
   });
 });
